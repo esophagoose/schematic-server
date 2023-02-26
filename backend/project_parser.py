14ff8b646a1a913446e91c2d6083c145fcbcf0ae
esophagoose <118142981+esophagoose@users.noreply.github.com>
2023-02-26 15:37:12 -0800

    Small backend refactor

--- backend/project_parser.py
+++ backend/project_parser.py
@@ -7,6 +7,7 @@ import glob
 import logging
 from typing import Dict
 from backend.schematic import Schematic
+from pathlib import Path
 
 logging.basicConfig(level=logging.DEBUG)
 @dataclass
@@ -17,42 +18,31 @@ class ProjectFile:
 
 class Project:
     def __init__(self, root_folder) -> None:
-        self.root = root_folder
+        self.root = Path(root_folder)
         self.parser = configparser.ConfigParser()
         self.schematics: Dict[str, Schematic] = {}
     
-    def read(self, filepath: Optional[str] = None) -> None:
-        if not filepath:
-            filepath = glob.glob(f"{self.root}/*.PrjPCB")[0]
-        logging.info(f"Reading project file: {filepath}")
-        self.parser.read(filepath, encoding="latin1")
-
-    def get_projects(self):
-        projects = {}
-        for p in glob.glob(f"{self.root}/*/", recursive=True):
-            folders = [i for i in p.split("/") if i]
-            projects[folders[-1]] = p
-        return projects
+    def read(self) -> None:
+        filepath = [p for p in os.listdir(self.root) if p.lower().endswith(".prjpcb")][0]
+        full_path = self.root / Path(filepath)
+        logging.info(f"Reading project file: {full_path}")
+        self.parser.read(full_path, encoding="latin1")
+        return self
 
     def get_schematics(self):
         schematics = []
         for key, value in self.parser.items():
             if "Document" not in key:
                 continue
-            print(value)
             name, extension = os.path.splitext(value["documentpath"])
             if extension != ".SchDoc":
                 continue
-            exists = os.path.exists(f'{self.root}/{value["documentpath"]}')
+            exists = os.path.exists(self.root / value["documentpath"])
             schematics.append(ProjectFile(name, exists, value["documentpath"]))
         return schematics
     
     def get_variants(self):
         return ["[No Variation]"]
-
-    def get_schematic_as_base64(self, doc: ProjectFile) -> Schematic:
-        filepath = f"{self.root}/{doc.path}"
-        return Schematic().read(filepath)
 
     def get_schematic_json(self, path: str) -> Schematic:
         filepath = f"{self.root}/{path}.SchDoc"
--- main.py
+++ main.py
@@ -3,57 +3,38 @@ from typing import Dict
 from flask import render_template
 from flask import Flask
 import flask
-from backend.project_parser import Project
-import json
+from backend.backend import Backend
+import yaml
 import base64
 import logging
 
-app = Flask(__name__, template_folder="frontend/templates/")
-root_folder = '.'
-cache: Dict[str, Project] = {}
 
+CONFIG = yaml.safe_load(open("config.yaml"))
+ROOT_DIRECTORY = CONFIG["project_directory"]
+BACKEND = Backend(ROOT_DIRECTORY)
+app = Flask(__name__, template_folder="frontend/templates/")
 
 logging.basicConfig(level=logging.INFO)
 
 @app.route("/project/<name>")
-def project(name, variant=None, schematic=None):
-    if name not in cache:
-        cache[name] = Project(f"designs/{name}")
-        cache[name].read()
-
-    schematics = cache[name].get_schematics()
-    variants = cache[name].get_variants()
-
-    variant = int(flask.request.args.get('variant', 0))
-    schematic_index = int(flask.request.args.get('schematic', 0))
-
-    selected_schematic = schematics[schematic_index]
-    sch_binary = cache[name].get_schematic_as_base64(selected_schematic).records
+def project(name):
+    project = BACKEND.projects[name]
     return render_template(
         "project.html",
         name=name,
-        schematics=schematics,
-        variants=variants,
-        selected_schematic=schematic_index,
-        selected_variant=variant,
-        render_target=sch_binary
+        schematics=project.get_schematics(),
+        variants=project.get_variants()
     )
 
 @app.route("/schematic/<name>/<schematic>")
 def schematic(name, schematic: str):
-    if name not in cache:
-        cache[name] = Project(f"designs/{name}")
-        cache[name].read()
-    return cache[name].get_schematic_json(schematic).records
+    return BACKEND.projects[name].get_schematic_json(schematic).records
 
 @app.route("/image/<name>/<schematic>/<image_b64>")
 def image(name, schematic: str, image_b64: str):
-    if name not in cache:
-        cache[name] = Project(f"designs/{name}")
-        cache[name].read()
     path = base64.b64decode(image_b64[:-len(".bmp")])
     app.logger.info(f"Requesting image: {path}")
-    image_binary = cache[name].get_image(schematic, path)
+    image_binary = BACKEND.projects[name].get_image(schematic, path)
     response = flask.make_response(image_binary)
     response.headers.set('Content-Type', 'image/bmp')
     response.headers.set('Content-Disposition', 'attachment', filename=image_b64)
@@ -61,16 +42,6 @@ def image(name, schematic: str, image_b64: str):
 
 @app.route("/")
 def main():
-    return render_template("index.html", structure=Project(f"designs").get_projects().keys());
-    # for root, dirs, files in os.walk(root_folder):
-    #     for file in files:
-    #         if not file.endswith(".PrjPCB") and not dirs:
-    #             continue
-    #         print(root, dirs, file, os.path.isfile(file))
-            # folders = os.path.split(root)
-            # folders = folders[folders.index(root_folder)+1:]
-            # i = 0
-            # while i < len(folders): 
-            #     if 
-            # if i == 0 
+    projects = BACKEND.get_projects().keys()
+    return render_template("index.html", structure=projects);
 
